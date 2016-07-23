eee6d746ab10ec2406cae63aa3fe58b7b0eac622
Jan van Heesch <TheVaan@gmail.com>
2016-07-23 10:29:17 +0200

    Fixes #2
    - TankDK wird gebuilded
    - Hunter wird gebuilded (beim öffnen kommt es zu Fehlern, da Icons für
    pet Abilitys nicht gefunden werden)
    - Warlock wird gebuilded
    - ProtPala wird gebuilded

--- Rawr.TankDK/Properties/AssemblyInfo.cs
+++ Rawr.TankDK/Properties/AssemblyInfo.cs
@@ -34,5 +34,3 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.3.23.0")]
 [assembly: AssemblyFileVersion("2.3.23.0")]
-
-[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Rawr.UnitTests")]
